2c08f18e1b3b59c232f4dc3ceefb9928f35b88f8
vijjuk <khandelwalvijendra@gmail.com>
2026-01-05 22:31:19 +0530

    chore: update package.json dependencies and add start script; modify Sonner imports; enhance Vite config with plugins and server settings

--- vite.config.ts
+++ vite.config.ts
@@ -62,10 +62,16 @@
   //   },
   // });
 
-  import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist'
-  }
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
